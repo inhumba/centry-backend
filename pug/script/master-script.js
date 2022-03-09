97041e0bbaef9e8a06d5d53237e282df7945e27e
Pathompong Sangkajit <p.inhumba@gmail.com>
2022-03-09 10:37:31 +0700

    Initial Project

---
+++ pug/script/master-script.js
@@ -0,0 +1,8 @@
+
+<script src="../../assets/vendor/jquery/dist/jquery.min.js"></script>
+    <script async src="../../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script async src="../../assets/vendor/js-cookie/js.cookie.js"></script>
+    <script src="../../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+    <script src="../../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+    <!-- <script src="../../assets/vendor/bootstrap-select-1.13.14/dist/js/bootstrap-select.min.js"></script> -->
+    <script defer src="../../assets/js/monito.js?v=1.2.0"></script>
